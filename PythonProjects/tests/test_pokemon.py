603a8a9d929ef36f62d35ba3d8cec424949ff1e9
YanaYakovlevaQA <yanna2013yanna@gmail.com>
2024-11-04 14:44:09 +0500

    Update test_pokemon.py

--- PythonProjects/tests/test_pokemon.py
+++ PythonProjects/tests/test_pokemon.py
@@ -2,9 +2,9 @@ import requests
 import pytest
 
 URL = "https://api.pokemonbattle.ru/v2"
-TOKEN = "52ac9979326cd22743c069cb8a76bc74"
+TOKEN = "8008d69203afa307f07e3faa03f2cc34"
 HEADER = {"Content-Type": "application/json", "trainer_token": TOKEN }
-TRAINER_ID = "7313"
+TRAINER_ID = "7382"
 
 def test_status_code():
 
@@ -13,4 +13,4 @@ def test_status_code():
 
 def test_part_of_response(): 
       response_get = requests.get(url = f"{URL}/trainers", params = {"trainer_id": TRAINER_ID})  
-      assert response_get.json()["data"][0]["trainer_name"] == "котопес"
+      assert response_get.json()["data"][0]["trainer_name"] == "YanaYk"
